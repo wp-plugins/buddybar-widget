9e37c3cb1e79b77b60153674ca068710eceb38bc
slushman <slushman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-14 04:09:42 +0000

    Adding files meant to be in the previous commit.
    git-svn-id: https://plugins.svn.wordpress.org/buddybar-widget/tags/0.21@1220552 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/partials/buddybar-widget-login-form.php
@@ -0,0 +1,20 @@
+<?php
+
+
+do_action( 'bp_before_sidebar_login_form' );
+
+?><p id="login-text"><?php
+
+	esc_html_e( 'To edit your profile, please log in.', 'buddybar-widget' );
+
+?></p><?php
+
+wp_login_form();
+
+if ( bp_get_signup_allowed() ) {
+
+	printf( wp_kses( __( '<a href="%s" title="Create an account">Create an account</a> to log in.', 'buddybar-widget' ), array( 'a' => array( 'href' => array(), 'title' => array() ) ) ), bp_get_signup_page() );
+
+} // End of bp_get_signup_allowed check
+
+do_action( 'bp_after_sidebar_login_form' );
